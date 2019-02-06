95d9095713a82737abba6c57506ae95274b34fa1
Obilarius <git@walzenbach.com>
2019-02-06 08:43:52 +0100

    Add project files.

---
+++ MouseTransition/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace MouseTransition
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_m01width = new System.Windows.Forms.TextBox();
+            this.txt_m01height = new System.Windows.Forms.TextBox();
+            this.txt_m02height = new System.Windows.Forms.TextBox();
+            this.txt_m02width = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "BallonTipText";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "MouseTransition";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showSettingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 48);
+            // 
+            // showSettingsToolStripMenuItem
+            // 
+            this.showSettingsToolStripMenuItem.Name = "showSettingsToolStripMenuItem";
+            this.showSettingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.showSettingsToolStripMenuItem.Text = "Settings";
+            this.showSettingsToolStripMenuItem.Click += new System.EventHandler(this.showSettingsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Monitor 01";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Monitor 02";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "x";
+            // 
+            // txt_m01width
+            // 
+            this.txt_m01width.Location = new System.Drawing.Point(22, 171);
+            this.txt_m01width.Name = "txt_m01width";
+            this.txt_m01width.Size = new System.Drawing.Size(100, 20);
+            this.txt_m01width.TabIndex = 5;
+            // 
+            // txt_m01height
+            // 
+            this.txt_m01height.Location = new System.Drawing.Point(22, 210);
+            this.txt_m01height.Name = "txt_m01height";
+            this.txt_m01height.Size = new System.Drawing.Size(100, 20);
+            this.txt_m01height.TabIndex = 6;
+            // 
+            // txt_m02height
+            // 
+            this.txt_m02height.Location = new System.Drawing.Point(237, 210);
+            this.txt_m02height.Name = "txt_m02height";
+            this.txt_m02height.Size = new System.Drawing.Size(100, 20);
+            this.txt_m02height.TabIndex = 9;
+            // 
+            // txt_m02width
+            // 
+            this.txt_m02width.Location = new System.Drawing.Point(237, 171);
+            this.txt_m02width.Name = "txt_m02width";
+            this.txt_m02width.Size = new System.Drawing.Size(100, 20);
+            this.txt_m02width.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(280, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(12, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "x";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(387, 140);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Save / Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 282);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_m02height);
+            this.Controls.Add(this.txt_m02width);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_m01height);
+            this.Controls.Add(this.txt_m01width);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Settings";
+            this.ShowInTaskbar = false;
+            this.Text = "Settings";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_m01width;
+        private System.Windows.Forms.TextBox txt_m01height;
+        private System.Windows.Forms.TextBox txt_m02height;
+        private System.Windows.Forms.TextBox txt_m02width;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MouseTransition/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseTransition
+{
+    public partial class Settings : Form
+    {
+        public Size monitor01 = new Size(1920, 600);
+        public Size monitor02 = new Size(1920, 1200);
+
+        Thread loopThread;
+
+        public Settings()
+        {
+            InitializeComponent();
+
+            var _allScreens = Screen.AllScreens;
+
+            if (_allScreens[0].Bounds.X == 0)
+            {
+                monitor01.Width = _allScreens[0].Bounds.Width;
+                monitor01.Height = _allScreens[0].Bounds.Height;
+                monitor02.Width = _allScreens[1].Bounds.Width;
+                monitor02.Height = _allScreens[1].Bounds.Height;
+            }
+            else if (_allScreens[1].Bounds.X == 0)
+            {
+                monitor01.Width = _allScreens[1].Bounds.Width;
+                monitor01.Height = _allScreens[1].Bounds.Height;
+                monitor02.Width = _allScreens[0].Bounds.Width;
+                monitor02.Height = _allScreens[0].Bounds.Height;
+            }
+
+            txt_m01width.Text = Convert.ToString(monitor01.Width);
+            txt_m01height.Text = Convert.ToString(monitor01.Height);
+            txt_m02width.Text = Convert.ToString(monitor02.Width);
+            txt_m02height.Text = Convert.ToString(monitor02.Height);
+
+            loopThread = new Thread(Loop);
+            loopThread.Start();
+        }
+
+        public int ConvertRange (int originalStart, int originalEnd, int newStart, int newEnd, int value)
+        {
+
+            int originalDiff = originalEnd - originalStart;
+            int newDiff = newEnd - newStart;
+            float ratio = (float)newDiff / (float)originalDiff;
+            int newProduct = Convert.ToInt16(value * ratio);
+            int finalValue = newProduct + newStart;
+            return finalValue;
+
+        }
+
+        public void Loop ()
+        {
+            var last = new Point();
+
+            while (true)
+            {
+                var c = Control.MousePosition;
+                //lbl_out.Text = "X: " + c.X + " - Y: " + c.Y;
+                //Console.WriteLine("X: " + c.X + " - Y: " + c.Y);
+
+                if (last.X >= monitor01.Width && c.X <= monitor01.Width)
+                {
+                    //this.Cursor = new Cursor(Cursor.Current.Handle);
+                    int newY = ConvertRange(0, monitor02.Height, 0, monitor01.Height, c.Y);
+
+                    //Console.WriteLine("Alt: " + c.Y + " - Neu: " + newY);
+
+                    Cursor.Position = new Point(c.X, newY);
+                }
+
+                if (last.X <= monitor01.Width && c.X >= monitor01.Width)
+                {
+                    //this.Cursor = new Cursor(Cursor.Current.Handle);
+                    int newY = ConvertRange(0, monitor01.Height, 0, monitor02.Height, c.Y);
+
+                    //Console.WriteLine("Alt: " + c.Y + " - Neu: " + newY);
+
+                    Cursor.Position = new Point(c.X, newY);
+                }
+
+                last.X = c.X;
+                last.Y = c.Y;
+
+                Thread.Sleep(3);
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                monitor01.Width = Convert.ToInt16(txt_m01width.Text);
+                monitor01.Height = Convert.ToInt16(txt_m01height.Text);
+                monitor02.Width = Convert.ToInt16(txt_m02width.Text);
+                monitor02.Height = Convert.ToInt16(txt_m02height.Text);
+
+                notifyIcon1.Icon = SystemIcons.Application;
+                notifyIcon1.BalloonTipText = "MouseTransition has minimized to tray";
+                notifyIcon1.ShowBalloonTip(1000);
+                this.Hide();
+            }
+            else if (this.WindowState == FormWindowState.Normal)
+            {
+                txt_m01width.Text = Convert.ToString(monitor01.Width);
+                txt_m01height.Text = Convert.ToString(monitor01.Height);
+                txt_m02width.Text = Convert.ToString(monitor02.Width);
+                txt_m02height.Text = Convert.ToString(monitor02.Height);
+            }
+        }
+
+        private void showSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            loopThread.Abort();
+            Application.Exit();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
